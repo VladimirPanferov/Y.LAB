362cac87c5bd75fec95487ae5e28c929ece6aec2
Vladimir Panferov <v3nseagal@gmail.com>
2022-07-12 00:35:12 +0700

    Interactions changes

--- y_lab/summer_2022/HW_3/task3_refactoring /save_places.py
+++ y_lab/summer_2022/HW_3/task3_refactoring /save_places.py
@@ -1,16 +1,24 @@
+from heroes import SuperHero
+
 from places import AbstractCity
+
+from news import Media
 
 
 class SavePlace:
-    def __init__(self, hero, place: AbstractCity, media) -> None:
+    """Interactions between hero and place"""
+
+    def __init__(self, hero: SuperHero, place: AbstractCity, media: Media) -> None:
         self.hero = hero
         self.place = place
         self.media = media
 
     def find(self) -> str:
+        """Find antagonist in the place"""
         return self.place.get_antagonist()
 
     def save_the_place(self) -> None:
+        """Hero save the place"""
         self.find()
         self.hero.attack()
-        self.media.create_news()
+        self.media.create_news(f"Hero {self.hero.name} save {self.place.name}.")
